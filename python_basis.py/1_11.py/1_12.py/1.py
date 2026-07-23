35e8b44ee623dab9e6e1870e4760a3c60d0e2c68
Poonam Dhami <poonamdhami07@gmail.com>
2026-07-23 20:04:50 +0530

    Deleted old Python files

--- python_basis.py/1_11.py/1_12.py/1.py
+++
@@ -1,9 +0,0 @@
-#Write a Python program that takes a number as input and determines whether it is positive, negative, or zero. Use if, else, and elif statements for decision-making. 
-number = int(input("Enter a number: "))
-
-if number > 0:
-    print("The number is positive.")
-elif number < 0:
-    print("The number is negative.")
-else:
-    print("The number is zero.")
--- python_basis.py/1_11.py/1_12.py/2.py
+++
@@ -1,7 +0,0 @@
-#Write a Python program that takes a user's age as input and prints whether they are eligible to vote or not (considering the legal voting age is 18). 
-age = int(input("Enter your age: "))
-
-if age >= 18:
-    print("You are eligible to vote.")
-else:
-    print("You are not eligible to vote.")
